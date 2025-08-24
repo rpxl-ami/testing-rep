d9e98d5af4bc0b68c98848530243887b95960012
rpxl-ami <amit.dutt@rpxl.com>
2025-08-24 16:51:02 +0530

    String attribute in js file

---
+++ alert.js
@@ -0,0 +1,17 @@
+//console.log("hello");
+//console.log(2+8);
+
+const userName = new String("A blue owl with red eye.");
+
+// Key Value Pair
+console.log(`Key Value Pair at 5th position: ${userName[5]}`);
+
+// Length
+console.log(`User Name String Length is: ${userName.length}`);
+
+//at
+let index = 0;
+console.log(`An index of ${index} have value ${userName.at(index)}`);
+
+let newIndex = -3;
+console.log(`An negitive index of ${newIndex} value of ${userName.at(newIndex)}`);
