be82b67ab88fe0bb61bfc9d9e6fbe9c082c2e907
rpxl-ami <amit.dutt@rpxl.com>
2025-08-29 18:07:51 +0530

    array done

---
+++ array.js
@@ -0,0 +1,15 @@
+const myArry = [9,8,7,6,5,4,-2]
+const myArry2 = [12, 13, 14, 15, 16, 17]
+myArry.push(15); // add element at the end of array
+myArry.pop(); // remove last elemnt from the array
+
+//const joinArray = myArry.concat(myArry2);
+
+//console.log(myArry.at(3));
+//console.log(myArry);
+
+//const iterator = myArry.entries();
+//console.log(iterator.next().value);
+//console.log(iterator.next().value);
+
+//console.log(myArry.copyWithin(2, 4, 5));
