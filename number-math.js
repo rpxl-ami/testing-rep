6cdb2c0b6d1085e95dba75731e727e6151226891
rpxl-ami <amit.dutt@rpxl.com>
2025-08-29 18:06:23 +0530

    number & dates done

---
+++ number-math.js
@@ -0,0 +1,24 @@
+let numb = 325;
+//console.log(`Number is: ${numb}`);
+let newNumb = new Number(745);
+//console.log(`The New Number is: ${newNumb}`);
+
+let presNumb = 12345.67890;
+
+//console.log(`Precision no 1: ${presNumb.toPrecision(1)}`);
+//console.log(`Precision no 2: ${presNumb.toPrecision(2)}`);
+//console.log(`Precision no 3: ${presNumb.toPrecision(3)}`);
+//console.log(`Precision no 4: ${presNumb.toPrecision(4)}`);
+//console.log(`Precision no 5: ${presNumb.toPrecision(5)}`);
+//console.log(`Precision no 6: ${presNumb.toPrecision(6)}`);
+//console.log(`Precision no 7: ${presNumb.toPrecision(7)}`);
+
+const numb1 = 350000;
+
+//console.log(numb1.toLocaleString("en-DE"));
+
+// tofixed()
+//console.log(`TO Fixed method: ${presNumb.toFixed()}`);
+
+// tostring()
+console.log(numb1.toString().length);
