d8dc7d9c6453f285b05114c94ec7aa326dfc3c04
rpxl-ami <amit.dutt@rpxl.com>
2025-09-02 22:38:01 +0530

    Spread Syntex check

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 const myArry = [9,8,7,6,5,4,-2]
 const myArry2 = [12, 13, 14, 15, 16, 17]
-myArry.push(15); // add element at the end of array
-myArry.pop(); // remove last elemnt from the array
+//myArry.push(15); // add element at the end of array
+//myArry.pop(); // remove last elemnt from the array
 
 //const joinArray = myArry.concat(myArry2);
 
@@ -13,3 +13,7 @@ myArry.pop(); // remove last elemnt from the array
 //console.log(iterator.next().value);
 
 //console.log(myArry.copyWithin(2, 4, 5));
+
+// Spread syntaz test for object. Key should be different otherwise it will override the value of the key.
+const syntaxArray = {...myArry, ...myArry2};
+console.log(syntaxArray);
